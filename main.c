e2035e00980e2073fe1c6344d10ad13f4d444d86
raul2710 <raultavares27@outlook.com>
2024-04-08 00:26:44 -0300

    show signal input

--- main.c
+++ main.c
@@ -1,44 +1,3 @@
-
-//
-//#define q	3		/* for 2^3 points */
-////#define N	(1<<q)		/* N-point FFT, iFFT */
-//		//====================================================================================	
-//		
-//	    int N = 8;
-//		Comp *sig, *f, *sig0;
-//		
-//		sig = (Comp *)malloc(sizeof(Comp) * (size_t)N);
-//	    sig0 = (Comp *)malloc(sizeof(Comp) * (size_t)N);
-//	    f = (Comp *)malloc(sizeof(Comp) * (size_t)N);
-//    
-//		int k;
-//		
-//		for(k=0; k<N; k++) {
-//			sig[k].a = sin(2*PI*50*k/N)+sin((2*PI*70*k/N)+PI/4);
-//			sig[k].b = 0;
-//		}
-//		
-//		test_fft(sig, f, sig0, N);
-//    	float vector_teste[N];
-//    	system("pause");
-//		//fft_magnitude(f, vector_teste, N);
-//		
-//		fft_magnitude(f, vector_teste, N);
-//		
-//		int yw, xw, fft_graphic_len = 100;
-//    	for (yw = 0; yw < N*fft_graphic_len; yw++){
-//    		printf("%f\n", vector_teste[yw]);
-//    		for(xw=0; xw< vector_teste[yw/fft_graphic_len]*30; xw++ )
-//    			DrawPixel(200+yw, 500-xw, 0xffffff);
-//		}
-//        
-//		
-//		// Desenhando o sinal de entrada	
-//		float jjk;
-//		for(jjk=0; jjk<800; jjk+=0.01) {
-//			DrawPixel(jjk+200, (50*sin(2*PI*50*jjk/10000)+50*sin((2*PI*200*jjk/10000)+PI/4)+200), 0xffffff);
-//		}
-
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdint.h>
@@ -76,22 +35,24 @@ void ClearScreen(u32 Color) {
 }
 
 void draw_fft(HDC DeviceContext, Comp *y, int n, int width, int height) {
-    double x_step = (double)width / n;
+    double x_step = (double)width / n;	// Faz divide o comprimento pela quantidade de amostra
     double max_amplitude = 0;
     
+    // Escreve o título acima no centro da tela
     RECT textRect;
 	DrawText(DeviceContext,TEXT("Gráfico Magnitude com dominio da frequncia"),-1,&textRect, DT_CENTER | DT_NOCLIP);     
     
-    // Encontra a amplitude máxima
+    // Encontra a amplitude máxima e pega o módulo
     int i;
     for (i = 0; i < n / 2; i++) {
+    	// Tirando o módulo do vetor imaginário e pegando a amplitude máxima
         double amplitude = sqrt(y[i].a * y[i].a + y[i].b * y[i].b);
         if (amplitude > max_amplitude)
             max_amplitude = amplitude;
     }
 
-    // Desenha o gráfico
-    MoveToEx(DeviceContext, 0, height, NULL); // Movendo para a posição inicial do gráfico
+    // Faz o gráfico na tela
+    MoveToEx(DeviceContext, 0, height, NULL); // Coloca no inicio do gráfico
     for (i = 0; i < n / 2; i++) {
         double x = i * x_step;
         double amplitude = sqrt(y[i].a * y[i].a + y[i].b * y[i].b);
@@ -100,11 +61,30 @@ void draw_fft(HDC DeviceContext, Comp *y, int n, int width, int height) {
         // Normaliza a posição y para o intervalo [0, height-1]
         y_pos = fmax(0, fmin(y_pos, height - 1));
 
-        // Desenha uma linha até o próximo ponto do gráfic
+        // Coloca a cor da linha como branca
         SelectObject(DeviceContext,GetStockObject(WHITE_PEN));
         
+        // Desenha uma linha até o próximo ponto do gráfico
+        // DrawPixel(x, y_pos, 0xFFFFFF);
+
         LineTo(DeviceContext, x, y_pos);
     }
+}
+
+void draw_signal(HDC DeviceContext, Comp *y, int n, int width, int height) {
+    double x_step = (double)width / n;	// Faz divide o comprimento pela quantidade de amostra
+    double max_amplitude = 0;
+    
+    // Faz o gráfico na tela
+    MoveToEx(DeviceContext, 0, y[1].a * 100 + 300, NULL); // Coloca no inicio do gráfico
+    // Desenhando o sinal de entrada
+    int i;
+	for (i=0; i<n/2; i++)	
+    	LineTo(DeviceContext, i*10, y[i].a * 100 + 300);
+//		float i;
+//		for(i=0; i<800; i+=0.01) {
+//			DrawPixel(i+200, y[i].a);
+//		}
 }
 
 
@@ -168,27 +148,33 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR Cmd, int Cm
 
     HDC DeviceContext = GetDC(Window);
     
-    // Definindo a amostragem e tempo
+    // Define as frequencias da onda 1 e 2 que serão somadas
+    int f1 = 50;
+    int f2 = 200;
+    
+    // Definindo o tamanho da amostragem
     int amostragem = 1000;
     
-    double *t = malloc(601 * sizeof(double));
+    // Número de amostras
+    int N = 601; 
     
-    int N = 601; // Número de amostras
+    // Cria o vetor tempo
+    double *t = malloc(N * sizeof(double)); 
     
-    Comp *s = malloc(N * sizeof(Comp));
+    // Cria o vetor para o sinal
+    Comp *signal = malloc(N * sizeof(Comp));		
 
-    // Construindo o sinal
+    // Constrói o sinal e armazena na variavel signal
     int i;
     for (i = 0; i < N; i++) {
         t[i] = (double)i / amostragem;
-        s[i] = comp_create(sin(2 * M_PI * 50 * t[i]) + sin(2 * M_PI * 200 * t[i] + M_PI / 4), 0);
+        signal[i] = comp_create(sin(2 * M_PI * f1 * t[i]) + sin(2 * M_PI * f2 * t[i] + M_PI / 4), 0);
     }
 
-    // Definindo o FFT
-    Comp *y = malloc(N * sizeof(Comp));
-    fft(s, y, 1, N, 0);
+    // Faz o cálculo da transformada e armazena o resultado na variável signalOut
+    Comp *signalOut = malloc(N * sizeof(Comp));
+    fft(signal, signalOut, 1, N, 0);
 	
-	i = 0;
     for (;;) {
         MSG Message;
         if (PeekMessage(&Message, NULL, 0, 0, PM_REMOVE)) {
@@ -200,22 +186,28 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR Cmd, int Cm
 		
         // Limpa a tela
         ClearScreen(0x333333);
-		int color = 0xFFFFFFFF;
-        // Desenha o FFT
+
+        // Desenha o sinal de entrada	
+		float jjk;
+		for(jjk=0; jjk<800; jjk+=0.01) {
+			DrawPixel(jjk+200, (50*sin(2*M_PI*50*jjk/10000)+50*sin((2*M_PI*200*jjk/10000)+M_PI/4)+200), 0xffffff);
+		}
         
-     
-        for (i=0; i<100; i++)
-        	draw_fft(DeviceContext, y, N, ClientWidth, ClientHeight);
+        // Desenha o os pontos de amostragem
+        //draw_signal(DeviceContext, signal, N, ClientWidth, ClientHeight);
+        
+        // Desenha o gráfico da transformada
+		draw_fft(DeviceContext, signalOut, N, ClientWidth, ClientHeight);
 		
         // Mostra a imagem
         StretchDIBits(DeviceContext, 0, 0, ClientWidth, ClientHeight, 0, 0, ClientWidth, ClientHeight, Memory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
         
     }
 
-    // Liberando a memória
+    // Libera a memória utilizada
     free(t);
-    free(s);
-    free(y);
+    free(signal);
+    free(signalOut);
 
     return 0;
 }
